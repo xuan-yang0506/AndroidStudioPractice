10c671590000fda715837807156057e11ab32a9f
xuany2 <xuany2@uw.edu>
2020-04-02 19:29:41 -0700

    lecture 4.2

--- app/src/main/java/edu/washington/xuany2/jackbottv/MainActivity.kt
+++ app/src/main/java/edu/washington/xuany2/jackbottv/MainActivity.kt
@@ -4,18 +4,45 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private var randomCode: Int = Random.nextInt(1000, 10000)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvRandomCode.text = tvRandomCode.toString()
+
+        val tvRandomCode : TextView = findViewById<TextView>(R.id.tvRandomCode);
+        tvRandomCode.text = randomCode.toString()
+
+        btnCancel.setOnClickListener { btnCancelBtn: View ->
+            cancelButtonClicked(btnCancelBtn)
+        }
+    }
+
+    private fun cancelButtonClicked(view: View) {
+        etCode.text.clear()
     }
 
     fun submitClicked(view: View) {
-        Log.i("hahah", "bbb");
 
-        Toast.makeText(this, "You got mail!", Toast.LENGTH_SHORT).show();
+        val etCodeText : String = etCode.text.toString()
+
+        if (etCodeText.isNotEmpty()) {
+            if (etCodeText == randomCode.toString()) {
+                Toast.makeText(this, "Name: You're in", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Try again", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
